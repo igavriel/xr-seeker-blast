e8e6ca5c5afee4fb1ef337ac19a67604c1cbfb77
Ilan Gavriel <igavriel@users.noreply.github.com>
2026-02-03 11:06:30 +0200

    shooting

--- Assets/Scripts/RayGun.cs
+++ Assets/Scripts/RayGun.cs
@@ -2,19 +2,18 @@ using UnityEngine;
 
 public class RayGun : MonoBehaviour
 {
-    public OVRInput.RawButton shootingButton;
-    public LineRenderer laserPrefab;
-    public Transform shootingPoint;
-    public float maxDistance = 5f;
-    public float laserShowTimer = 0.3f;
+    [SerializeField] public OVRInput.RawButton shootingButton;
+    [SerializeField] public LineRenderer laserPrefab;
+    [SerializeField] public Transform shootingPoint;
+    [SerializeField] public float maxDistance = 5f;
+    [SerializeField] public float laserShowTimer = 0.3f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Util.AssertObject(laserPrefab, "Laser prefab is not assigned");
-        Util.AssertObject(shootingPoint, "Shooting point is not assigneπd");
-        //Util.AssertObject(shootingButton, "Shooting button is not assigned");
+        Util.AssertObject(shootingPoint, "Shooting point is not assigned");
     }
 
     // Update is called once per frame
@@ -26,12 +25,12 @@ public class RayGun : MonoBehaviour
         }
     }
 
-    void Shoot()
+    public void Shoot()
     {
         LineRenderer laser = Instantiate(laserPrefab);
         laser.positionCount = 2;
         laser.SetPosition(0, shootingPoint.position);
-        Vector3 endPoint = shootingPoint.position + transform.forward * maxDistance;
+        Vector3 endPoint = shootingPoint.position + shootingPoint.forward * maxDistance;
         laser.SetPosition(1, endPoint);
         Destroy(laser.gameObject, laserShowTimer);
     }
